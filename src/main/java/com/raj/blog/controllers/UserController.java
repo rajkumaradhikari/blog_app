522fe528efbba171c9d6c980d4b482eabc6ab694
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2025-01-03 00:52:36 +0545

    bean validation added for user entity

--- src/main/java/com/raj/blog/controllers/UserController.java
+++ src/main/java/com/raj/blog/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.raj.blog.controllers;
 import com.raj.blog.payloads.ApiResponse;
 import com.raj.blog.payloads.UserDto;
 import com.raj.blog.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,12 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/post")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
         UserDto createUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
         UserDto updatedUser = this.userService.updateUser(userDto, uid);
         return ResponseEntity.ok(updatedUser);
     }
--- src/main/java/com/raj/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/raj/blog/exceptions/GlobalExceptionHandler.java
@@ -4,8 +4,13 @@ package com.raj.blog.exceptions;
 import com.raj.blog.payloads.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +20,16 @@ public class GlobalExceptionHandler {
         String message = exception.getMessage();
         ApiResponse apiResponse = new ApiResponse(message, false);
         return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> MethodArgumentNotValidException(MethodArgumentNotValidException exception){
+         Map<String, String> resp = new HashMap<>();
+         exception.getBindingResult().getAllErrors().forEach((error)->{
+             String fieldName = ((FieldError)error).getField();
+             String message = error.getDefaultMessage();
+             resp.put(fieldName,message);
+         });
+
+         return new ResponseEntity<Map<String, String>>(resp,HttpStatus.BAD_REQUEST);
     }
 }
