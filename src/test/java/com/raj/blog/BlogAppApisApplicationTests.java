8707e228af0262ede6825442c9569b1f59ad8cd2
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2025-01-02 14:29:40 +0545

    writing test case for interface autowire check

--- src/test/java/com/raj/blog/BlogAppApisApplicationTests.java
+++ src/test/java/com/raj/blog/BlogAppApisApplicationTests.java
@@ -1,13 +1,26 @@
 package com.raj.blog;
 
+import com.raj.blog.repositories.UserRepo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogAppApisApplicationTests {
 
+	@Autowired
+	private UserRepo userRepo;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void repoTest(){
+		String className = this.userRepo.getClass().getName();
+		System.out.println("==================="+className);
+		String packageName = this.userRepo.getClass().getPackageName();
+		System.out.println("==================="+packageName);
 	}
 
 }
