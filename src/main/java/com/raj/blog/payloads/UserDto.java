05b85c8df4a3cb69c779156f443caaab32499195
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2025-02-04 23:34:44 +0545

    test

--- src/main/java/com/raj/blog/payloads/UserDto.java
+++ src/main/java/com/raj/blog/payloads/UserDto.java
@@ -3,7 +3,6 @@ package com.raj.blog.payloads;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
